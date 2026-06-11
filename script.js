0c7b2d9908e8f17bda5a1c36d30061ced5498a79
Rx Coder / <rxcoderbd@gmail.com>
2026-06-11 17:22:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Javascript Loops Learning Code 
 
+// This Code Is Your Phone or Laptop Hang and Lag For This Code //
+
+// Please Safely Use This Code This Code Is Very Denger For Your Phone or Laptop //
+
+for (let screenlock = 1; screenlock <= -0; screenlock++){
+    alert("Sorrrrrrrry 🙏🏻, Your Phone or Laptop is totally Hang 😁");
+    let password = prompt("Stop This Lag Please Enter The Stop Password 🔑 ");
+    if(password === "stop") {
+        alert("Your Phone Lag Is S Stop ✅");
+        break;
+    }else {
+        alert("Sorry Wrong Password Please Try Again ⛔");
+    }
+}
